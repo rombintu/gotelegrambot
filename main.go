c0df0a27e6c6b3cee45c170b8d3c48fe2145a3bb
Nick <romb.intu@gmail.com>
2021-09-25 00:01:08 +0300

    0.0.3

--- main.go
+++ main.go
@@ -40,46 +40,79 @@ func main() {
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = conf.Default.TimeoutUpdate
 
+	// _, err = bot.SetWebhook(tgbotapi.NewWebhook("https://webhook.site/14a926fb-364d-46ee-b01c-15e69130f41b/" + bot.Token))
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// updates := bot.ListenForWebhook("/" + bot.Token)
+	// go http.ListenAndServe("0.0.0.0:5000", nil)
+
+	// for update := range updates {
+	// 	log.Printf("%+v\n", update)
+	// }
+
 	updates, err := bot.GetUpdatesChan(u)
 	if err != nil {
 		log.Fatalf("%v", err)
 	}
 
-	for u := range updates {
-
-		if u.Message == nil {
+	for update := range updates {
+		if update.Message == nil {
 			continue
 		}
 
-		userText := u.Message.Text
-		userID := u.Message.Chat.ID
-		// userLang := u.Message.From.LanguageCode
-		// userName := u.Message.From.UserName
+		userText := update.Message.Text
+		userCommand := update.Message.Command()
+		userID := update.Message.Chat.ID
 
-		printBot := func(text string) {
-			message := tgbotapi.NewMessage(userID, text)
-			if _, err := bot.Send(message); err != nil {
-				log.Printf("ERROR SEND: %v", err)
+		// log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		if update.Message.IsCommand() {
+			msg := tgbotapi.NewMessage(userID, "")
+			switch userCommand {
+			case "start":
+				msg.ReplyMarkup = botKeyboard
+				msg.Text = "Hello"
+			case "help":
+				msg.Text = "type /sayhi or /status."
+			case "sayhi":
+				msg.Text = "Hi :)"
+			case "status":
+				msg.Text = "I'm ok."
+			case "withArgument":
+				msg.Text = "You supplied the following argument: " + update.Message.CommandArguments()
+			case "html":
+				msg.ParseMode = "html"
+				msg.Text = "This will be interpreted as HTML, click <a href=\"https://www.example.com\">here</a>"
+			default:
+				msg.Text = "I don't know that command"
 			}
-		}
-
-		returnKeyboard := func(text string) {
-			message := tgbotapi.NewMessage(userID, text)
-			message.ReplyMarkup = botKeyboard
-			if _, err := bot.Send(message); err != nil {
-				log.Printf("ERROR RETURN KEYBOARD: %v", err)
+			bot.Send(msg)
+		} else if update.Message.Document != nil {
+			doc := update.Message.Document
+			userFileID := doc.FileID
+			// userFileName := doc.FileName
+			msg := tgbotapi.NewDocumentShare(userID, userFileID)
+			bot.Send(msg)
+		} else {
+			msg := tgbotapi.NewMessage(userID, "")
+			switch userText {
+			case "button1":
+				msg.Text = "press button1"
+			case "button2":
+				photoBytes, photoName := tools.ReadFileFromUploads()
+				newFileConfig := tgbotapi.FileBytes{
+					Name:  photoName,
+					Bytes: photoBytes,
+				}
+				msg.Text = photoName
+				bot.Send(tgbotapi.NewPhotoUpload(userID, newFileConfig))
+			default:
+				msg.Text = "I don't know that command"
 			}
-		}
-
-		switch userText {
-		case "/start":
-			returnKeyboard("hello!")
-		case "button1":
-			printBot("button1")
-		case "button2":
-			printBot("button2")
-		default:
-			printBot("Неизвестная команда")
+			bot.Send(msg)
 		}
 	}
+
 }
--- tools/tools.go
+++ tools/tools.go
@@ -1,9 +1,14 @@
 package tools
 
 import (
+	"bufio"
+	"log"
 	"os"
+	"path"
+	"path/filepath"
 
 	"github.com/BurntSushi/toml"
+	uuid "github.com/satori/go.uuid"
 )
 
 type Default struct {
@@ -31,4 +36,31 @@ func ParseConfigToml(path string) (*Config, error) {
 	}
 
 	return &conf, nil
+}
+
+func GetNewUuid() string {
+	return uuid.NewV4().String()
+}
+
+func ReadFileFromUploads() ([]byte, string) {
+	currentDir, err := os.Getwd()
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+	uploadsDir := filepath.Join(currentDir, "uploads")
+	file, err := os.Open(path.Join(uploadsDir, "example.jpeg"))
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+	defer file.Close()
+
+	fileInfo, _ := file.Stat()
+	bytes := make([]byte, fileInfo.Size())
+	buffer := bufio.NewReader(file)
+	_, err = buffer.Read(bytes)
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+
+	return bytes, GetNewUuid()
 }
